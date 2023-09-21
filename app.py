ef6ca265d8b44a8d3062906dde4200e72abdd149
Joe Booth <joe@joebooth.com>
2023-09-20 20:37:50 -0700

    add main loop at 30fps

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
+import asyncio
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import socketio
 import uvicorn
 
-sio = socketio.AsyncServer(cors_allowed_origins="*", async_mode="asgi")
+
+chat_history = []
+debug_info = []
+user_typing_feed = ""
+steps = 0
+
+
 app = FastAPI()
+
+async def emit_debug():
+    global steps
+    global debug_info
+    debug_info = []
+    debug_info.append(f"---- debug info ----")
+    debug_info.append(f"typing_in_progress: {user_typing_feed}")
+    debug_info.append(f"steps: {steps}")
+    await sio.emit("update_debug", debug_info)
+
+async def main_loop():
+    global steps
+    global debug_info
+    while True:
+        # Your main loop code here
+        steps += 1
+
+        await emit_debug()
+        
+        # Sleep for 1/30th of a second to achieve 30 FPS
+        await asyncio.sleep(1 / 30)
+
+# Start the main loop when the application starts
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(main_loop())
+
+sio = socketio.AsyncServer(cors_allowed_origins="*", async_mode="asgi")
 sio_asgi_app = socketio.ASGIApp(socketio_server=sio, other_asgi_app=app)
 
 app.add_middleware(
@@ -15,8 +50,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-chat_history = []
-debug_info = []
 
 @sio.event
 async def connect(sid, environ):
@@ -24,15 +57,13 @@ async def connect(sid, environ):
 
 @sio.event
 async def typing_in_progress(sid, data):
-    # debug_info.append(data)
-    debug_info = [
-        "\n"
-        f"typing_in_progress: {data}"
-        ]
-    await sio.emit("update_debug", debug_info)
+    global user_typing_feed
+    user_typing_feed=data
 
 @sio.event
 async def complete_sentence(sid, data):
+    global user_typing_feed
+    user_typing_feed = ""
     chat_history.append(data)
     await sio.emit("update_chat", chat_history)
 
